0b5b87d3bf1c231ca097743543bdabbb0998ce98
Anushautti <94513817+Anushautti@users.noreply.github.com>
2022-10-10 15:14:57 +0530

    U.Anusha created Priority.c

---
+++ OSLAB/Priority.c
@@ -0,0 +1,6 @@
+/*Name Of the Candidate: U.Anusha
+   Roll No: 204G1A0514
+   Title of the Experiment: CPU Scheduling Using Priority Algorithm
+   Date of Creation: 29-08-2022
+   Date of Execution: 29-08-2022
+*/
