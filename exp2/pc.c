62f3923eb7ad6a662e2d42721ba236f6246f3196
204G1A0514 <94513817+Anushautti@users.noreply.github.com>
2022-10-11 11:32:59 +0530

    U.Anusha created pc.c

---
+++ exp2/pc.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: U.Anusha
+   Roll No: 204G1A0501
+   Title of the Experiment: CPU Scheduling Using producer-consumer Problem
+   Date of Creation: 11-10-2022
+   Date of Execution: 11-10-2022
+*/
