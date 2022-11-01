0298284e993d92d4d4d1e40145ee5541533d33fa
204G1A0514 <94513817+Anushautti@users.noreply.github.com>
2022-11-01 12:50:34 +0530

    U.Anusha created FIFO.c

---
+++ Exp6/FIFO.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: U.Anusha
+   Roll No: 204G1A0501
+   Title of the Experiment: implementing FIFO Algorithm
+   Date of Creation: 01-11-2022
+   Date of Execution: 01-11-2022
+*/
